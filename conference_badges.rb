83650a8bc29defb21a1a21a99fca80edc5b02fd3
Jeremy Bourne <jbourne25@yahoo.com>
2020-01-24 04:35:53 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+#speaker_order = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(speaker)
+  "Hello, my name is #{speaker}."
+end 
+
+def batch_badge_creator(speaker_order)
+  speaker_order.map{ |speaker| badge_maker(speaker) }
+end 
+
+def assign_rooms(speaker_list)
+  speaker_list.each_with_index.map{ |speaker, index| "Hello, #{speaker}! You'll be assigned to room #{} "
+
+  
